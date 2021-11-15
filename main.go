be37ce43b337bb293ea18cf65c7051ea180e9358
Carlos A Becker <caarlos0@gmail.com>
2021-11-15 16:53:46 -0300

    feat: simplified config

--- main.go
+++ main.go
@@ -1,55 +1,44 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 
-	"github.com/caarlos0/env/v6"
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
 	"github.com/charmbracelet/wish"
 	bm "github.com/charmbracelet/wish/bubbletea"
 	lm "github.com/charmbracelet/wish/logging"
 	"github.com/gliderlabs/ssh"
 	"github.com/maaslalani/confetty/confetti"
 	"github.com/maaslalani/confetty/fireworks"
-	"github.com/muesli/termenv"
 )
 
-type Config struct {
-	Host   string `env:"CONFETTYSH_HOST" envDefault:"127.0.0.1"`
-	Port   int    `env:"CONFETTYSH_PORT" envDefault:"2222"`
-	Effect string `env:"CONFETTYSH_EFFECT" envDefault:"confetti"`
-}
+var port = flag.Int("port", 2222, "port to listen on")
+var effect = flag.String("effect", "confetti", "effect to use [confetti|fireworks]")
 
 func main() {
-	var cfg Config
-	if err := env.Parse(&cfg); err != nil {
-		log.Fatalln(err)
-	}
-
-	// force colors as we might start it from systemd which has no interactive term and no colors
-	lipgloss.SetColorProfile(termenv.ANSI256)
+	flag.Parse()
 
 	s, err := wish.NewServer(
-		wish.WithAddress(fmt.Sprintf("%s:%d", cfg.Host, cfg.Port)),
+		wish.WithAddress(fmt.Sprintf("0.0.0.0:%d", *port)),
 		wish.WithHostKeyPath(".ssh/confettysh"),
 		wish.WithMiddleware(
-			bm.Middleware(teaHandler(cfg)),
+			bm.Middleware(teaHandler()),
 			lm.Middleware(),
 		),
 	)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	log.Printf("Starting SSH server on %s:%d", cfg.Host, cfg.Port)
+	log.Printf("Starting SSH server on 0.0.0.0:%d", *port)
 	err = s.ListenAndServe()
 	if err != nil {
 		log.Fatalln(err)
 	}
 }
 
-func teaHandler(cfg Config) func(s ssh.Session) (tea.Model, []tea.ProgramOption) {
+func teaHandler() func(s ssh.Session) (tea.Model, []tea.ProgramOption) {
 	return func(s ssh.Session) (tea.Model, []tea.ProgramOption) {
 		_, _, active := s.Pty()
 		if !active {
@@ -58,13 +47,13 @@ func teaHandler(cfg Config) func(s ssh.Session) (tea.Model, []tea.ProgramOption)
 		}
 
 		var m tea.Model
-		switch cfg.Effect {
+		switch *effect {
 		case "confetti":
 			m = confetti.InitialModel()
 		case "fireworks":
 			m = fireworks.InitialModel()
 		default:
-			log.Fatalf("invalid effect %q", cfg.Effect)
+			log.Fatalf("invalid effect %q", *effect)
 		}
 
 		return m, []tea.ProgramOption{tea.WithAltScreen()}
