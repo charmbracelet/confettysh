88f66df4ecc719a3c62c0fc3401fb52aa07fcf69
Carlos A Becker <caarlos0@gmail.com>
2022-01-11 10:03:43 -0300

    feat: use wishlist
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>

--- main.go
+++ main.go
@@ -4,67 +4,85 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"os"
-	"os/signal"
-	"syscall"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/promwish"
 	"github.com/charmbracelet/wish"
-	"github.com/charmbracelet/wish/accesscontrol"
 	"github.com/charmbracelet/wish/activeterm"
 	bm "github.com/charmbracelet/wish/bubbletea"
 	lm "github.com/charmbracelet/wish/logging"
+	"github.com/charmbracelet/wishlist"
 	"github.com/gliderlabs/ssh"
 	"github.com/maaslalani/confetty/confetti"
 	"github.com/maaslalani/confetty/fireworks"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 var port = flag.Int("port", 2222, "port to listen on")
 var metricsPort = flag.Int("metrics-port", 9222, "port to listen on")
-var effect = flag.String("effect", "confetti", "effect to use [confetti|fireworks]")
+
+const (
+	effectConfetti  = "confetti"
+	effectFireworks = "fireworks"
+)
 
 func main() {
 	flag.Parse()
 
-	s, err := wish.NewServer(
-		wish.WithAddress(fmt.Sprintf("0.0.0.0:%d", *port)),
-		wish.WithHostKeyPath(".ssh/confettysh"),
-		wish.WithMiddleware(
-			bm.Middleware(teaHandler()),
-			lm.Middleware(),
-			promwish.Middleware(fmt.Sprintf("0.0.0.0:%d", *metricsPort), *effect),
-			accesscontrol.Middleware(),
-			activeterm.Middleware(),
-		),
-	)
-	if err != nil {
-		log.Fatalln(err)
+	go promwish.Listen(fmt.Sprintf("0.0.0.0:%d", *metricsPort))
+
+	cfg := &wishlist.Config{
+		Port: int64(*port),
+		Factory: func(e wishlist.Endpoint) (*ssh.Server, error) {
+			return wish.NewServer(
+				wish.WithAddress(e.Address),
+				wish.WithMiddleware(
+					append(
+						e.Middlewares,
+						promwish.MiddlewareRegistry(
+							prometheus.DefaultRegisterer,
+							prometheus.Labels{
+								"app": e.Name,
+							},
+						),
+						lm.Middleware(),
+						activeterm.Middleware(),
+					)...,
+				),
+			)
+		},
+		Endpoints: []*wishlist.Endpoint{
+			{
+				Name: effectConfetti,
+				Middlewares: []wish.Middleware{
+					bm.Middleware(teaHandler(effectConfetti)),
+				},
+			},
+			{
+				Name: effectFireworks,
+				Middlewares: []wish.Middleware{
+					bm.Middleware(teaHandler(effectFireworks)),
+				},
+			},
+		},
 	}
-	log.Printf("Starting SSH server on 0.0.0.0:%d", *port)
-	done := make(chan os.Signal, 1)
-	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
-	go func() {
-		if err = s.ListenAndServe(); err != nil {
-			log.Fatalln(err)
-		}
-	}()
-	<-done
-	if err := s.Close(); err != nil {
+
+	// start all the servers
+	if err := wishlist.Serve(cfg); err != nil {
 		log.Fatalln(err)
 	}
 }
 
-func teaHandler() func(s ssh.Session) (tea.Model, []tea.ProgramOption) {
+func teaHandler(effect string) func(s ssh.Session) (tea.Model, []tea.ProgramOption) {
 	return func(s ssh.Session) (tea.Model, []tea.ProgramOption) {
 		var m tea.Model
-		switch *effect {
-		case "confetti":
+		switch effect {
+		case effectConfetti:
 			m = confetti.InitialModel()
-		case "fireworks":
+		case effectFireworks:
 			m = fireworks.InitialModel()
 		default:
-			log.Fatalf("invalid effect %q", *effect)
+			log.Fatalf("invalid effect %q", effect)
 		}
 
 		return m, []tea.ProgramOption{tea.WithAltScreen()}
